9845ae1779008c51be2df3da585c363b309337b2
Gideao Lucas <gideaolu025@gmail.com>
2024-10-09 21:48:18 -0300

    commit :qa	'

--- envia.php
+++ envia.php
@@ -1,21 +1,19 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $emaill = addcslashes($_POST['emaill']);
-    $celular = addcslashes($_POST['celular']);
-    $mensagem = addcslashes($_POST['mensagem']);
+$nome = addcslashes($_POST['nome']);
+$emaill = addcslashes($_POST['emaill']);
+$celular = addcslashes($_POST['celular']);
+$mensagem = addcslashes($_POST['mensagem']);
 
-    $para = "diegoaxelbsr@gmail.com";
-    $assunto = "Coleta de dados - Site_Portifólio";
+$para = "gideaolu025@gmail.com";
+$assunto = "Coleta de dados - Site_Portifólio";
 
-    $corpo = "Nome: ".$nome."\n"."E-mail ".$emaill."\n"."Celular: ".$celular."\n"."Mensagem: ".$mensagem.
+$corpo = "Nome: " . $nome . "\n" . "E-mail " . $emaill . "\n" . "Celular: " . $celular . "\n" . "Mensagem: " . $mensagem .
 
-    $cabeca = "From: diego.axel.bernardo.097@ufrn.edu"."\n"."Reply-to: ".$emaill."\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: diego.axel.bernardo.097@ufrn.edu" . "\n" . "Reply-to: " . $emaill . "\n" . "X=Mailer:PHP/" . phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o e-mail!");
-    }
-
-?>
+if (mail($para, $assunto, $corpo, $cabeca)) {
+    echo ("E-mail enviado com sucesso!");
+} else {
+    echo ("Houve um erro ao enviar o e-mail!");
+}
